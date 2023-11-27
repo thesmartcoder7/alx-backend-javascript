7de3b0cded9076b4446c8aa4eda68c7c5d859fc2
samuel martins <samuel.martins4.sm@gmail.com>
2023-11-28 02:44:28 +0300

    chore: update and test the create iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,32 +1,9 @@
 export default function createIteratorObject(report) {
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+  const itObject = [];
 
-  const iterator = {
-    next() {
-      if (
-        currentDepartmentIndex >= report.getNumberOfDepartments(report.allEmployees)
-      ) {
-        return { done: true };
-      }
+  for (const value of Object.values(report.allEmployees)) {
+    itObject.push(...value);
+  }
 
-      const currentDepartment = Object.keys(report.allEmployees)[
-        currentDepartmentIndex
-      ];
-
-      const currentEmployees = report.allEmployees[currentDepartment];
-
-      if (currentEmployeeIndex >= currentEmployees.length) {
-        // Move to the next department
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-        return this.next(); // Recursively call next for the next department
-      }
-
-      const currentEmployee = currentEmployees[currentEmployeeIndex++];
-      return { value: currentEmployee, done: false };
-    },
-  };
-
-  return iterator;
+  return itObject;
 }
